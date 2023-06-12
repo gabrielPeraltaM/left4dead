6febeddddb31c0152a8208bb850d1cf9dedefc6f
ignacio <igarciap@fi.uba.ar>
2023-06-12 17:39:11 -0300

    cambio origen de las texturas de los backgrounds

--- client/game/GameRenderer/BackgroundRenderer.h
+++ client/game/GameRenderer/BackgroundRenderer.h
@@ -16,13 +16,13 @@ private:
     Renderer &renderer;
     std::vector<Character> &characters;
     Texture floor = Texture(renderer,
-                            "/home/ignacio/Documents/GitHub/sdl_prueba_cliente/background/road.png");
+                            RESOURCE_PATH "/background/road.png");
     Texture sky = Texture(renderer,
-                          "/home/ignacio/Documents/GitHub/sdl_prueba_cliente/background/sky.png");
+                          RESOURCE_PATH "/background/sky.png");
     Texture wall = Texture(renderer,
-                           "/home/ignacio/Documents/GitHub/sdl_prueba_cliente/background/wall.png");
+                           RESOURCE_PATH "/background/wall.png");
     Texture houses = Texture(renderer,
-                             "/home/ignacio/Documents/GitHub/sdl_prueba_cliente/background/houses.png");
+                             RESOURCE_PATH "/background/houses.png");
     const int LEVEL_WIDTH = floor.GetWidth();
     Rect camera = {0, 0, LEVEL_WIDTH / 2, floor.GetHeight()};
     Rect skyCamera = {0, 0, LEVEL_WIDTH / 2, floor.GetHeight()};
